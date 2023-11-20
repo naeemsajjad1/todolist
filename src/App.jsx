be6402896543c4b501c77dad19f6fc41bd55986a
naeemsajjad1 <you@example.com>
2023-11-20 14:20:40 +0100

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,41 @@ import './index.css';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode((prevDarkMode) => !prevDarkMode);
   };
 
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible((prevVisible) => !prevVisible);
+  };
+
   return (
     <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
-      <nav className="navbar">
+      {/* Regular desktop navbar */}
+      <nav className="navbar desktop-navbar">
         <div className="navbar-brand">My Todo App</div>
-        <ul className="nav-links">
+        <ul className={`nav-links ${mobileMenuVisible ? 'hidden' : ''}`}>
           <li><a href="#home">Home</a></li>
           <li><a href="#contactus">Contact Us</a></li>
           <li><a href="#aboutus">About Us</a></li>
         </ul>
+        {/* Mobile toggle button for smaller screens */}
+        <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+          ☰
+        </button>
       </nav>
+
+      {/* Mobile navbar for smaller screens */}
+      <nav className={`navbar mobile-navbar ${mobileMenuVisible ? 'visible' : ''}`}>
+        <a href="#home" onClick={toggleMobileMenu}>Home</a>
+        <a href="#contactus" onClick={toggleMobileMenu}>Contact Us</a>
+        <a href="#aboutus" onClick={toggleMobileMenu}>About Us</a>
+      </nav>
+
+      {/* Main content */}
       <div className="homepage">
-        {/* Existing sections */}
         <section className="section">
           <h2 className='todoabout'>About Todo List</h2>
           <p className='naeemtodo'>Naeem Sajjad-Todo-List</p>
